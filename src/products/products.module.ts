d4b8d6b134735929b8b09270a1dd49f4eb534e42
abdurrehman99 <syed.ebad1999@gmail.com>
2020-11-15 19:18:16 +0500

    category data in getAllProd

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,10 +4,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { ProductSchema } from './../models/Product.schema';
+import { CategorySchema } from './../models/Category.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+    MongooseModule.forFeature([{ name: 'Category', schema: CategorySchema }]),
   ],
   controllers: [ProductsController],
   providers: [ProductsService],
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -9,21 +9,25 @@ import {
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Product } from '../models/Product.schema';
+import { Category } from '../models/Category.schema';
 import ResponseMsgs from '../utils/ResponseMsgs';
 
 @Injectable()
 export class ProductsService {
   constructor(
     @InjectModel('Product') private readonly productsModel: Model<Product>,
+    @InjectModel('Category') private readonly categoriesModel: Model<Category>,
   ) {}
 
   /******* Get All products *******/
   async getAllProducts() {
     const products = await this.productsModel.find();
+    const categories = await this.categoriesModel.find();
     throw new HttpException(
       {
         statusCode: HttpStatus.OK,
         products,
+        categories,
       },
       HttpStatus.OK,
     );
